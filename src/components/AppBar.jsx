6f16643c03734038933b24edb93d29ab0bafb4df
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-24 08:40:50 +0200

    Add logout functionality 10.16

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,7 +3,12 @@ import { View, StyleSheet, Pressable, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import Text from './Text';
 import theme from '../theme';
-import { Link } from 'react-router-native';
+import { Link, Redirect } from 'react-router-native';
+import { useQuery } from '@apollo/client';
+import { CHECK_USER } from '../qraphql/queries';
+
+import useAuthStorage from '../hooks/useAuthStorage';
+import { useApolloClient } from '@apollo/client';
 
 const styles = StyleSheet.create({
   container: {
@@ -29,12 +34,37 @@ const AppBarTab = ({ text, route }) => (
   </View>
 );
 
+const Logout = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+
+  const handleLogout = async () => {
+    authStorage.removeAccessToken();
+    apolloClient.resetStore();
+  };
+  return (
+    <View style={styles.tab}>
+      <Pressable onPress={handleLogout}>
+        <Text style={styles.text}>Sign out</Text>
+      </Pressable>
+    </View>
+  );
+};
+
 const AppBar = () => {
+  const loggedIn = useQuery(CHECK_USER);
+  if (loggedIn.loading) {
+    return (
+      <Text>Loading..</Text>
+    );
+  }
   return (
   <View style={styles.container}>
     <ScrollView horizontal>
       <AppBarTab text="Repositories" route="/" />
-      <AppBarTab text="Sign in" route="/login" />
+      {loggedIn.data.authorizedUser 
+        ? <Logout />
+        : <AppBarTab text="Sign in" route="/login" />}
     </ScrollView>
   </View>
   );
--- src/qraphql/queries.js
+++ src/qraphql/queries.js
@@ -19,3 +19,12 @@ export const GET_REPOSITORIES = gql`
     }
   }
 `;
+
+export const CHECK_USER = gql`
+  {
+    authorizedUser {
+      id
+      username
+    }
+  }
+`;
