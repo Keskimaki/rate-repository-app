0defc70c2af014ce4a287065f3bdae4dbbb381b2
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-24 12:18:34 +0200

    Add review list to single repository view 10.20

--- src/components/RepositoryDetails.jsx
+++ src/components/RepositoryDetails.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import Text from "./Text";
+import { View, FlatList, StyleSheet } from "react-native";
 import { useParams } from "react-router";
 import { useQuery } from "@apollo/client";
 import RepositoryItem from "./RepositoryItem";
 import { GET_REPOSITORY } from "../qraphql/queries";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 15,
+    backgroundColor: theme.colors.repositoryItem,
+  },
+  rating: {
+    color: theme.colors.primary,
+    borderColor: theme.colors.primary,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 15,
+    width: 30,
+    height: 30,
+    textAlign: 'center',
+    paddingTop: 4,
+    marginLeft: 10
+  }
+});
 
 const RepositoryDetails = () => {
   const { id } = useParams();
@@ -12,9 +38,47 @@ const RepositoryDetails = () => {
   if (repository.loading) {
     return <Text>Loading...</Text>;
   }
-  
+
   return (
-    <RepositoryItem repository={repository.data.repository} detailed={true} />
+    <View>
+      <RepositoryItem repository={repository.data.repository} detailed={true} />
+      <ReviewList reviews={repository.data.repository.reviews.edges} />
+    </View>
+  );
+};
+
+const ReviewItem = ({ review }) => {
+  const parseDate = date => date.substring(0, date.indexOf('T')).split('-').reverse().join('.');
+  return (
+    <View style={styles.container}>
+      <View style={{flexDirection: 'row'}}>
+        <View>
+          <Text fontWeight="bold">{review.user.username}</Text>
+          <Text color="textSecondary">{parseDate(review.createdAt)}</Text>
+        </View>
+        <Text style={styles.rating} fontWeight="bold">{review.rating}</Text>
+      </View>
+      <Text>{review.text}</Text>
+    </View>
+  );
+};
+
+const ReviewList = ( {reviews} ) => {
+  const reviewNodes = reviews.map(edge => edge.node);
+
+  const ItemSeparator = () => <View style={styles.separator} />;
+
+  const renderItem = (review) => (
+    <ReviewItem review={review.item} />
+  );
+
+  return (
+    <FlatList
+      data={reviewNodes}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={renderItem}
+      ListHeaderComponent={() => <ItemSeparator />}
+  />
   );
 };
 
